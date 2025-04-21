67e87d2902f114caa4b7d376a6b982ad8782696e
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2025-04-21 20:28:27 +1000

    dd

--- api/handler.ts
+++ api/handler.ts
@@ -1,11 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-const THEYVOTEFORYOU_API_KEY = process.env.THEYVOTEFORYOU_API_KEY;
-
 export default function handler(
     request: VercelRequest,
     response: VercelResponse,
 ) {
+    const THEYVOTEFORYOU_API_KEY = process.env.THEYVOTEFORYOU_API_KEY;
+
     fetch(`https://theyvoteforyou.org.au/api/v1/people/${request.query.id}.json?key=${THEYVOTEFORYOU_API_KEY}`)
         .then((response) => response.json())
         .then((data) => {
